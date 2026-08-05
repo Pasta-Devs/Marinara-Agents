177b7de2cc8bb43dc5701f225f7db739f8afd99b
Gunterlie <hey@gunterlie.de>
2026-08-05 17:23:46 +0200

    build(virtual-phone): publish v1.0.4 package

--- scripts/build-feature-packages.mjs
+++ scripts/build-feature-packages.mjs
@@ -144,7 +144,7 @@ const features = [
   },
   {
     id: "virtual-phone",
-    version: "1.0.3",
+    version: "1.0.4",
     minEngineVersion: "2.4.1",
     maxEngineExclusive: MAX_ENGINE_EXCLUSIVE,
     name: "Virtual Phone",
